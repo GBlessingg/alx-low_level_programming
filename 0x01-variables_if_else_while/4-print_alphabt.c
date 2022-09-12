f8d790fff3215174145a5e93b17aa107aa0bff50
GBlessingg <61010252+GBlessingg@users.noreply.github.com>
2022-09-13 00:21:16 +0100

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,7 +1,16 @@
 #!/usr/bin/env perl
 
+#include <stdio.h>
 use strict;
 use warnings;
 use File::Basename;
-{ 
+int main()
+{
+
+    int i;
+    for (i = 'a'; i<='z'; i++)
+    {
+        printf("%C\n", i);
+    }
+    return (0);
 }
