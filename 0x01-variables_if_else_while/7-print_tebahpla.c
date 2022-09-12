45e2a3a2fa94b10cb4cd58e94feed8bc2a4ca462
GBlessingg <61010252+GBlessingg@users.noreply.github.com>
2022-09-13 00:24:41 +0100

    Update 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,8 +1,16 @@
 #!/usr/bin/env perl
 
+#include <stdio.h>
 use strict;
 use warnings;
 use File::Basename;
+int main()
 {
-  
+
+    int i;
+    for (i = 'z'; i>='a'; i--)
+    {
+        printf("%C\n", i);
+    }
+    return (0);
 }
