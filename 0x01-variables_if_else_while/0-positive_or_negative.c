56e282c39ae91fc86dd4374c7b2f614e64949a89
GBlessingg <61010252+GBlessingg@users.noreply.github.com>
2022-09-12 20:27:00 +0100

    Create 0-positive_or_negative.c
    This program assign a random number to variable n whenever it's executed

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+  int n;
+  
+  srand(time(0));
+  n = rand() - RAND_MAX / 2;
+  /* your code */
+  return (0)
+}
