15213509324d83837bf7689bdcc452f7a68b53a9
GBlessingg <61010252+GBlessingg@users.noreply.github.com>
2022-09-13 00:17:14 +0100

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,8 +1,14 @@
 #!/usr/bin/env perl
-
+#include <stdio.h>
 use strict;
 use warnings;
 use File::Basename;
+int main()
 {
-  
+    int i, j;
+    for (i = 'A'; j= 'a'; i<='Z'; j<='z'; i++; j++)
+    {
+        printf("%C\n", i, j);
+    }
+    return (0);
 }
