044e811c0160239a136b570dfeb5c75aa9d66aad
GBlessingg <61010252+GBlessingg@users.noreply.github.com>
2022-09-13 00:46:53 +0100

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -6,10 +6,12 @@
 /* betty style doc for function main */
 int main(void)
 {
-  int n;
+  int n, d;
   
   srand(time(0));
   n = rand() - RAND_MAX /2;
-  /* code */
+  scanf("%d|. &n);
+  d = n % 10;
+  printf("The last digit of %d is: %d", n, d);
   return(0);
 }
