758478379186978cef80b27db57ba7bde9ccf71a
GBlessingg <61010252+GBlessingg@users.noreply.github.com>
2022-09-13 01:24:11 +0100

    Update 2-print_alphabet.c
    Program that print lowercase letters

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,8 +1,15 @@
 #!/usr/bin/env perl
-
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
 use strict;
 use warnings;
 use File::Basename;
+int main()
 {
-
+  int i;
+  for (i='a'; i<='z'; i++)
+  { printf("%\n", i);
+  }
+  return(0);
 }
