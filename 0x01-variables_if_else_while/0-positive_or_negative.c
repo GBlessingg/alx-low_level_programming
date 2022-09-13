8639ebcda23fe5e4cd3e450985cf47546a98742d
GBlessingg <61010252+GBlessingg@users.noreply.github.com>
2022-09-13 01:17:18 +0100

    Update 0-positive_or_negative.c
    Program to check positive and negative random number

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,14 +3,13 @@
 #include <time.h>
 #include <stdio.h>
 
-/* betty style doc for function main goes there */
 int main(void)
 {
   int n;
   
   srand(time(0));
   n = rand() - RAND_MAX / 2;
-  scanf("%d", &n);
+ 
   if (n > 0)
   printf ("%d is positive.", n);
   if (n < 0)
