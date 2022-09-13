6966f9a83976e7f6f2fe96d96fb17394c878ab27
GBlessingg <61010252+GBlessingg@users.noreply.github.com>
2022-09-13 01:15:06 +0100

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -3,6 +3,14 @@
 use strict;
 use warnings;
 use File::Basename;
+#include <stdio.h>
+int main()
 {
-  
+   int n;
+  printf("Enter the digit: ");
+  scanf("%d", &n);
+  printf("%x", n);
+  printf("\n");
+  printf("%X",n);
+  return (0);
 }
