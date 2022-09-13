8acc6ab22e69e3378db667b79708f189e5e51c08
GBlessingg <61010252+GBlessingg@users.noreply.github.com>
2022-09-13 01:49:12 +0100

    Update 9-print_comb.c
    Program that prints all possible combination of single digit numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,8 +1,32 @@
 #!/usr/bin/env perl
-
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
 use strict;
 use warnings;
 use File::Basename;
+void print(int *n)
 {
-  
+  int i;
+  for (i = 0; i < 10; i++)
+  printf("%d", n[i]);
+  printf("\n");
+}
+int main()
+{
+  int n[N];
+  int *ptr;
+  int temp;
+  int i, j, j;
+  for (j=1; j<=10; j++) {
+      for i = 0; i < n-1; i++) {
+      
+      temp = n[i];
+      n[i] = num[i+1];
+      num[i+1] = temp;
+      print(n, 10);
+      }
+
+  }
+  return (0);
 }
