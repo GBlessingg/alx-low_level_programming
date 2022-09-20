c80c68ba30eaf7cb4c7054b59f08e2e56175d4dc
GBlessingg <61010252+GBlessingg@users.noreply.github.com>
2022-09-21 00:17:08 +0100

    Create 9-strcpy.c
    Function that copies the string pointed to by src, including the terminating null byte (\0), to the buffer pointed to by dest

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* main - check the code for
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+  int array[5]
+    
+    char s1[98];
+    char *ptr;
+    
+    ptr = _strcpy(s1, "First solve the problem. Then, write the code\n");
+    printf("%s", s1);
+    printf("%s", s1);
+  
+  return (0);
+}
