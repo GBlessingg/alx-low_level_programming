6497e1c76df807bbec80b212278b04b461603547
GBlessingg <61010252+GBlessingg@users.noreply.github.com>
2022-09-21 01:41:44 +0100

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -5,6 +5,21 @@
 *
 * Return: Always 0.
 */
+void puts_half(str)
+{
+  char str[] = str;
+  char halfstr[];
+  int len, mid, i, k;
+  
+  len = strlen(str);
+  mid = len/2;
+  for (i = 0; i < mid; i++)
+  {
+    halfstr[i] = str[i];
+  }
+  halfstr[i] = '\0';
+  printf("First half of the string: %s\n", halfstr);
+}
 int main(void)
 {
   char *str;
