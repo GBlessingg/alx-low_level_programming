76f4dbf0cf2b30cf95757b1b53bef137e7b279e7
GBlessingg <61010252+GBlessingg@users.noreply.github.com>
2022-09-24 01:59:54 +0100

    Create 7-leet.c
    Function that encodes a string

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* main - check the code
+*
+* Return: Always 0.
+*/
+void leet() {
+  char s[]
+  int i=0;
+  while (s[i]!='\0')
+  {
+  if s[i] = 'L' || 'l' 
+    {
+      s[i] = 1;
+    }
+  if s[i] = 'E || 'e 
+    {
+      s[i] = 3;
+    }
+  if s[i] = 'T' || 't' 
+    {
+      s[i] = 7;
+    }
+}
+  printf("\n The coded string: %s", s);
+}
+int main(void)
+{
+  char s[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\n";
+  char *p;
+ 
+  p = leet(s);
+  printf("%s", p);
+  printf("%s", s);
+  
+  return 0;
+  )
